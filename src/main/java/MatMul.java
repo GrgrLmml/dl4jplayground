6ca945b983144752f05f0ce974346a6346d987ee
gla <gla@marketlogicsoftware.com>
2018-11-29 09:27:37 +0100

    initial

---
+++ src/main/java/MatMul.java
@@ -0,0 +1,39 @@
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+
+public class MatMul {
+
+    public static void main(String [] args) {
+
+
+        for (int i = 0; i < 300; i++) {
+            INDArray rnd = Nd4j.rand(i+1, 300);
+
+
+            INDArray mmul = rnd.mmul(rnd.transpose());
+            boolean isSymetric = testMatSym(mmul);
+            if (isSymetric) {
+                System.out.println("matrix is symmetric");
+            } else {
+                System.out.println("matrix is not symmetric");
+            }
+        }
+
+
+    }
+
+    private static boolean testMatSym(INDArray mmul) {
+        for (int i = 0; i < mmul.columns(); i++) {
+            for (int j=0; j<mmul.rows(); j++) {
+                double d1 = mmul.getFloat(i,j);
+                double d2 = mmul.getFloat(j,i);
+                if (d1 != d2) {
+                    System.out.println("diff: " + (d1-d2));
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
